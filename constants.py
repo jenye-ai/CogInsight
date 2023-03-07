5a1de990fdc28ab47949b91dc46815bb890fb8f7
Jennifer Ye <jenniferaiye@gmail.com>
2023-03-07 14:51:02 -0500

    created requirements.txt to make sure it works on all machines with same env.

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-VIDEO_PATH = "C:/Code/462ModelPipeline/test_video2.mp4"
-OUTPUT_DIR = "C:/Code/462ModelPipeline/results/"
+VIDEO_PATH = "test_video2.mov"
+OUTPUT_DIR = "results/"
 
 FRAME_RATE = 0.1
 IMAGE_PREFIX = "image"
@@ -7,8 +7,8 @@ FRAME_DIR = "original_frames"
 PROCESSED_DIR =  "processed_frames"
 
 #SmileCNN
-SMILECNN_PATH = "C:/Code/462ModelPipeline/video_models/smileCNN_iter2.pt"
-SVM_PATH = "C:/Code/462ModelPipeline/video_models/smileCNN_iter2_SVM.model"
+SMILECNN_PATH = "video_models/smileCNN_iter2.pt"
+SVM_PATH = "video_models/smileCNN_iter2_SVM.model"
 
 #Emonet
-EMONET_PATH = "C:/Code/462ModelPipeline/video_models/emonet_8.pth"
+EMONET_PATH = "video_models/emonet_8.pth"
